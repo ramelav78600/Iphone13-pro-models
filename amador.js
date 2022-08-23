5b8e7cd875534a18e497a26df925c550b80ca8bf
ramelav78600 <ramelav78600@gmail.com>
2022-08-23 18:25:37 +0530

    amdor file added

---
+++ amador.js
@@ -0,0 +1,14 @@
+const fetchPromise = fetch("https://ghibliapi.herokuapp.com/people");
+const main = document.getElementById("main");
+
+main.innerHTML = "<p>Loading...";
+
+fetchPromise.then(response => {
+    return response.json();
+}).then(people => {
+    main.innerHTML = listOfNames(people);
+});
+function listOfNames(people) {
+    const names = people.map(person => `<li>${person.name}</li>`).join("\n");
+    return `<ul>${names}</ul>`
+}
